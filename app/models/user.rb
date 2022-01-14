d1723092f8e92b135df5923c1eb749d43ed667b5
Malte Stiebritz <malte@nerdgeschoss.de>
2022-01-15 00:33:34 +0100

    Bis kurz vor listing 11.13

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
@@ -40,5 +41,18 @@ class User < ApplicationRecord
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # Converts email to all lower_case.
+  def downcase_email
+    email.downcase!
+  end
+
+  #Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.remember_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
