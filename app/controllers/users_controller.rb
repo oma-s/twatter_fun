c2524043e6428367287766ba92acbfd2eb57087b
Malte Stiebritz <malte@nerdgeschoss.de>
2022-01-31 15:05:00 +0100

    update logo, static pages, user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[index edit update destroy following followers]
+  before_action :logged_in_user, only: %i[index show edit update destroy following followers]
   before_action :correct_user,   only: %i[edit update]
   before_action :admin_user,     only: :destroy
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,8 @@ class Micropost < ApplicationRecord
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }
+  validates :content, presence: true, unless: Proc.new { |m| m.image.attached? }
   validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
                                       message: 'must be a valid image format' },
                       size: { less_than: 4.megabytes,
@@ -13,6 +14,6 @@ class Micropost < ApplicationRecord
 
   # Returns a resized image for display.
   def display_image
-    image.variant(resize_to_limit: [500, 300])
+    image.variant(resize_to_limit: [300, 300])
   end
 end
